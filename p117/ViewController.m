a507efc3d327149b39f6064b6d840b9c06f197e1
hoyeonKim <roadmaker@korea.ac.kr>
2014-01-06 14:13:46 +0900

    dissmissKeyboard
    배경터치로 키보드감추

--- p117/ViewController.m
+++ p117/ViewController.m
@@ -8,12 +8,73 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
+@interface ViewController ()<UITextFieldDelegate>
 
 @end
 
-@implementation ViewController
+@implementation ViewController{
+    int dy;
+}
+-(UITextField *)firstResponderTextField{
+    for(id child in self.view.subviews){
+        if([child isKindOfClass:[UITextField class]]){
+            UITextField *textField= (UITextField *)child;
+            if(textField.isFirstResponder){
+                return textField;
+            }
+            
+        }
+}
+    return nil;
+}
 
+- (IBAction)dissmissKeyboard:(id)sender {
+    [[self firstResponderTextField]resignFirstResponder];
+}
+
+-(void)keyboardWillShow:(NSNotification*)noti{
+    NSLog(@"keyboardWillShow, noti: %@",noti);
+    UITextField *firstResponder = (UITextField *)[self firstResponderTextField];
+    int y = firstResponder.frame.origin.y + firstResponder.frame.size.height+5;
+    int viewheight = self.view.frame.size.height;
+    
+    NSDictionary *userinfo = [noti userInfo];
+    CGRect rect= [[userinfo objectForKey:UIKeyboardFrameBeginUserInfoKey]CGRectValue];
+    int keyheight= (int)rect.size.height;
+    
+    float animationDuration = [[userinfo objectForKey:UIKeyboardAnimationDurationUserInfoKey]floatValue];
+    if(keyheight >(viewheight-y)){
+        NSLog(@"키보드가 가림");
+        [UIView animateWithDuration:animationDuration animations:^{dy=keyheight - (viewheight-y);
+            self.view.center = CGPointMake(self.view.center.x, self.view.center.y-dy);
+        }];
+    }
+    else{
+        NSLog(@"키보드가 가리지 않음");
+        dy=0;
+    }
+}
+-(void) keyHide:(NSNotification *)noti{
+    NSLog(@"keyboardWillHide");
+    
+    if(dy>0){
+        float animationDuration = [[[noti userInfo]objectForKey:UIKeyboardAnimationDurationUserInfoKey]floatValue];
+        [UIView animateWithDuration:animationDuration animations:^{
+            self.view.center=CGPointMake(self.view.center.x, self.view.center.y+dy);
+        }];
+        
+    }
+}
+
+-(void)viewDidAppear:(BOOL)animated{
+    [[NSNotificationCenter defaultCenter]addObserver:self selector:@selector(keyboardWillShow:) name:UIKeyboardWillShowNotification object:nil];
+    
+    [[NSNotificationCenter defaultCenter]addObserver:self selector:@selector(keyHide:) name:UIKeyboardWillHideNotification object:nil];
+}
+
+-(void) viewWillDisappear:(BOOL)animated{
+    [[NSNotificationCenter defaultCenter]removeObserver:self];
+}
 - (void)viewDidLoad
 {
     [super viewDidLoad];
